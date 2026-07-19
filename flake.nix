6ebc6970eb9095b4079bac043628685a95d3a926
DaikiMiwa <63869611+DaikiMiwa@users.noreply.github.com>
2026-07-19 21:12:36 +0900

    nixos: install home-manager cli

--- flake.nix
+++ flake.nix
@@ -348,6 +348,9 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "backup";
+            environment.systemPackages = [
+              home-manager.packages.${linuxSystem}.home-manager
+            ];
             home-manager.extraSpecialArgs = {
               inherit username;
               homeDirectory = "/home/${username}";
